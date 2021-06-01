aa0a2c696f6606d08dcece6c877c6288babb1530
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-06-01 20:41:12 +0700

    8.28 - 7.20 - 6.8

---
+++ 8kyu/8kyu 1/invert.js
@@ -0,0 +1,22 @@
+function invert(array) {
+   let hasil = [];
+   for (let i in array) {
+      array[i] > 0 ? array[i] = array[i] * -1 : array[i] -= array[i] * 2;
+      hasil.push(array[i]);
+   }
+   return hasil;
+}
+
+
+const invert = array => array.map(num => -num);
+
+
+function invert(array) {
+   return array.map(x => x === 0 ? x : -x);
+}
+
+
+console.log(invert([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5]);
+console.log(invert([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5]);
+console.log(invert([]), []);
+console.log(invert([0]), [0]);
