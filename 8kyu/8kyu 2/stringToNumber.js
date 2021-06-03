b9fe0827d4924aabbfa3000fcf6e6dab197e8cf9
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-06-03 20:11:34 +0700

    8.30 - 7.20 - 6.8

---
+++ 8kyu/8kyu 2/stringToNumber.js
@@ -0,0 +1,17 @@
+function stringToNumber(str) {
+   return Number(str);
+}
+
+
+const stringToNumber = str => parseInt(str);
+
+
+const stringToNumber = function (str) {
+   return +str;
+}
+
+
+console.log(stringToNumber("1234"), 1234)
+console.log(stringToNumber("605"), 605)
+console.log(stringToNumber("1405"), 1405)
+console.log(stringToNumber("-7"), -7)
