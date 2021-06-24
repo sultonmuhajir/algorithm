8d1a42121a3f305820084823e3deebbf8a9fc590
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-06-24 08:45:41 +0700

    8.37

---
+++ 8kyu 1/doubleInteger.js
@@ -0,0 +1,15 @@
+function doubleInteger(i) {
+   return i * 2;
+}
+
+
+const doubleInteger = i => i * 2;
+
+
+function doubleInteger(i) {
+   return i << 1;
+}
+
+
+console.log(doubleInteger(2), 4);
+console.log(doubleInteger(3), 6);
