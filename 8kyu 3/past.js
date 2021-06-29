b0939fee272143c780fd3d372885cef5191deb5f
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-06-29 11:27:29 +0700

    8.40

---
+++ 8kyu 3/past.js
@@ -0,0 +1,25 @@
+function past(h, m, s) {
+   return ((h * 3600000) + (m * 60000) + (s * 1000));
+}
+
+
+const past = (...args) => args.reduce((pre, val) => 60 * pre + val, 0) * 1e3;
+
+
+function past(h, m, s) {
+   return ((h * 3600) + (m * 60) + s) * 1000;
+}
+
+
+function past(h, m, s) {
+   const setTime = new Date().setHours(h, m, s);
+   const midnight = new Date().setHours(0, 0, 0);
+   return Math.round(setTime - midnight);
+}
+
+
+console.log(past(0, 1, 1), 61000)
+console.log(past(1, 1, 1), 3661000)
+console.log(past(0, 0, 0), 0)
+console.log(past(1, 0, 1), 3601000)
+console.log(past(1, 0, 0), 3600000)
