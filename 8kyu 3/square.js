a3d804724c9de65ded9f760f831a2f22782d802c
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-07-02 09:43:03 +0700

    8.42

---
+++ 8kyu 3/square.js
@@ -0,0 +1,25 @@
+function square(n) {
+   return n ** 2
+}
+
+
+const square = (n) => n * n;
+
+
+const square = function (number) {
+   return Math.pow(number, 2)
+}
+
+
+const square = (x) => {
+   return Math.sqrt(x ** 4);
+}
+
+
+function square(num) {
+   var num = Math.pow(num, 2);
+   return num;
+}
+
+
+console.log(square(3), 9);
