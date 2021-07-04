bdcf69c46d66dc68d24e1700ba63f834393c9539
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-07-04 20:43:46 +0700

    8.45

---
+++ 8kyu 2/numToStr.js
@@ -0,0 +1,16 @@
+const a = 123 + "";
+
+const a = String(123);
+
+const a = 123..toString();
+
+const a = new Number(123).toString();
+
+const a = 123["toString"]();
+
+const a = (123).toString();
+
+const a = `${123}`;
+
+
+console.log(a);
