a7c82a947bb56f81c431c5527703bb62377ff722
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-07-25 13:19:58 +0700

    8.53

---
+++ 8kyu 1/arrayPlusArray.js
@@ -0,0 +1,34 @@
+function arrayPlusArray(arr1, arr2) {
+   return arr1.reduce((acc, curr) => acc + curr) + arr2.reduce((acc, curr) => acc + curr);
+}
+
+
+function arrayPlusArray(arr1, arr2) {
+   return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
+
+
+function arrayPlusArray(arr1, arr2) {
+   let arr = [...arr1, ...arr2];
+   return arr.reduce((a, b) => a + b);
+}
+
+
+function arrayPlusArray(arr1, arr2) {
+   let r = 0;
+   for (let i = 0; i < arr1.length; i++) {
+      r += arr1[i] + arr2[i];
+   }
+   return r;
+}
+
+
+function arrayPlusArray(...arrays) {
+   return [].concat(...arrays).reduce((a, b) => a + b, 0)
+}
+
+
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]), 21);
+console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6]), -21);
+console.log(arrayPlusArray([0, 0, 0], [4, 5, 6]), 15);
+console.log(arrayPlusArray([100, 200, 300], [400, 500, 600]), 2100);
