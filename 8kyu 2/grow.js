8ae803e37a20dd1f6b73796d64436e9c799ce54e
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-07-28 10:16:58 +0700

    8.55

---
+++ 8kyu 2/grow.js
@@ -0,0 +1,29 @@
+function grow(x) {
+   return x.reduce((acc, curr) => acc * curr);
+}
+
+
+const grow = x => eval(x.join("*"))
+
+
+function grow(x) {
+   var product = 1;
+   x.forEach(function (e) {
+      product *= e
+   })
+   return product;
+}
+
+
+const grow = x => {
+   let res = 1;
+   for (let i = 0; i < x.length; i++) {
+      res *= x[i];
+   }
+   return res;
+};
+
+
+console.log(grow([1, 2, 3]), 6);
+console.log(grow([4, 1, 1, 1, 4]), 16);
+console.log(grow([2, 2, 2, 2, 2, 2]), 64);
