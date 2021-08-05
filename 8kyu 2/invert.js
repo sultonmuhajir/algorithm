3d37bf0a30eaa21c7eac27b94cb40f509a946677
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-08-05 19:27:35 +0700

    :fresh

--- 8kyu 2/invert.js
+++ 8kyu 2/invert.js
@@ -5,16 +5,6 @@
 |
 */
 
-function invert(array) {
-   let hasil = [];
-   for (let i in array) {
-      array[i] > 0 ? array[i] = array[i] * -1 : array[i] -= array[i] * 2;
-      hasil.push(array[i]);
-   }
-   return hasil;
-}
-
-
 const invert = array => array.map(num => -num);
 
 
@@ -23,7 +13,30 @@ function invert(array) {
 }
 
 
+const invert = a => a.map(n => ~~-n)
+
+
+function invert(array) {
+   return array.map(n => n && -n)
+}
+
+
+function invert(array) {
+   return array.map(e => e * -1);
+}
+
+
+// function invert(array) {
+//    let hasil = [];
+//    for (let i in array) {
+//       array[i] > 0 ? array[i] = array[i] * -1 : array[i] -= array[i] * 2;
+//       hasil.push(array[i]);
+//    }
+//    return hasil;
+// }
+
+
 console.log(invert([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5]);
 console.log(invert([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5]);
 console.log(invert([]), []);
-console.log(invert([0]), [0]);
+console.log(invert([0]), [-0]);
