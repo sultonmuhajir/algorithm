b8ce923fb3888136719da19a69ace2e15b597b0a
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-11-11 19:35:06 +0700

    84.69.83

---
+++ JavaScript/8js 2/otherAngle.js
@@ -0,0 +1,26 @@
+/*
+|--------------------------------------------------------------------------
+| Third Angle of a Triangle
+|--------------------------------------------------------------------------
+|
+*/
+
+function otherAngle(a, b) {
+   return 180 - (a + b);
+}
+
+
+function otherAngle(a, b) {
+   return 180 - a - b;
+}
+
+
+function otherAngle(a, b) {
+   return 180 - (a | 0) - (b | 0);
+}
+
+
+console.log(otherAngle(30, 60), 90);
+console.log(otherAngle(60, 60), 60);
+console.log(otherAngle(43, 78), 59);
+console.log(otherAngle(10, 20), 150);
