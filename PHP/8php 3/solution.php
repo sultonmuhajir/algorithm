ee6c4d6d5ed3bcd2fc140c2bbf5481859be4b94f
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-06-14 10:26:19 +0700

    144.125.144

---
+++ PHP/8php 3/solution.php
@@ -0,0 +1,27 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Classic Hello World
+|--------------------------------------------------------------------------
+|
+*/
+
+class Solution {
+   public static function main() {
+      echo 'Hello World!';
+   }
+}
+
+
+class Solution {
+   public function main() {
+      print 'Hello World!';
+   }
+}
+
+
+class Solution {
+   function main() {
+      echo "Hello World!";
+   }
+}
