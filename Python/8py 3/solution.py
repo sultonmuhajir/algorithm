616b4a6e870a2232dcb524f88dd3da546d17ba09
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-06-14 10:25:40 +0700

    144.124.144

---
+++ Python/8py 3/solution.py
@@ -0,0 +1,22 @@
+"""
+|--------------------------------------------------------------------------
+| Classic Hello World
+|--------------------------------------------------------------------------
+|
+"""
+
+class Solution:
+   def main(self):
+      print('Hello World!')
+
+
+class Solution:
+   @staticmethod
+   def main(self, *args):
+      print("Hello World!")
+
+
+class Solution(object):
+   @staticmethod
+   def main(*_):
+      print("Hello World!")
