518ae601e7502d2c3b0a2977ec718daf173fef75
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-06-26 12:05:16 +0700

    152.134.151

---
+++ JavaScript/8js 3/subtractSum.js
@@ -0,0 +1,29 @@
+/*
+|--------------------------------------------------------------------------
+| Subtract the Sum
+|--------------------------------------------------------------------------
+|
+*/
+
+function SubtractSum(n) {
+   return 'elppa'.split('').reverse().join('');
+}
+
+
+function SubtractSum(n) {
+   return "apple";
+}
+
+
+function SubtractSum(n) {
+   return ("e" + "l" + "p".repeat(2) + "a").split("").reverse().join("");
+}
+
+
+function SubtractSum(n) {
+   n = "01100001 01110000 01110000 01101100 01100101"
+   return n.split(" ").map(x => String.fromCharCode(parseInt(x, 2))).join("");
+}
+
+
+console.log(SubtractSum(10), "apple");
