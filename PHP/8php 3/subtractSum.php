bd035225f63c2c5bcddce835ca06c4011ed2b0e8
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-06-28 11:15:03 +0700

    152.135.152

---
+++ PHP/8php 3/subtractSum.php
@@ -0,0 +1,25 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Subtract the Sum
+|--------------------------------------------------------------------------
+|
+*/
+
+function subtractSum($n) {
+   $n = '0110000101110000011100000110110001100101';
+   return pack('H*', base_convert($n, 2, 16));
+}
+
+
+function subtractSum($n) {
+   return "apple";
+}
+
+
+function subtractSum($n) {
+   return implode('', array_reverse(str_split('elppa')));
+}
+
+
+print_r(SubtractSum(10) . " apple");
