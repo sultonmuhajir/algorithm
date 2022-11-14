da81aacdd10842397d39434bab9b9f8b88f30ff8
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-11-14 18:18:38 +0700

    162.141.162

---
+++ Python/8py 1/cockroach_speed.py
@@ -0,0 +1,22 @@
+"""
+|--------------------------------------------------------------------------
+| Beginner Series #4 Cockroach
+|--------------------------------------------------------------------------
+|
+"""
+
+def cockroach_speed(s):
+   return s/0.036//1
+
+
+def cockroach_speed(s):
+   return int(27.7778 * s)
+
+
+def cockroach_speed(s):
+   return s * 100000 // 3600
+
+
+print(cockroach_speed(1.08),30)
+print(cockroach_speed(1.09),30)
+print(cockroach_speed(0),0)
