7f9adcc5cf404a5f529c694a355ad96feb9783cf
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-11-27 10:57:11 +0700

    176.141.175

---
+++ JavaScript/8js 4/unusualFive.js
@@ -0,0 +1,28 @@
+/*
+|--------------------------------------------------------------------------
+| 5 without numbers !!
+|--------------------------------------------------------------------------
+|
+*/
+
+function unusualFive() {
+   return "abcde".length;
+}
+
+
+function unusualFive() {
+   return Math.hypot(Math.ceil(Math.PI), Math.floor(Math.PI));
+}
+
+
+function unusualFive() {
+   return ((!"" << !"") << !"") | !"";
+}
+
+
+function unusualFive() {
+   return ~(~(~false ^ ((true << true) << true) ^ ~false) ^ true);
+}
+
+
+console.log(unusualFive(), 5);
