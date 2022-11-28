25714b14e2eef93f02ea77d5adecd62a7ab82e53
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-11-28 09:29:48 +0700

    176.141.176

---
+++ Python/8py 4/unusual_five.py
@@ -0,0 +1,20 @@
+"""
+|--------------------------------------------------------------------------
+| 5 without numbers !!
+|--------------------------------------------------------------------------
+|
+"""
+
+def unusual_five():
+   return len("abcde")
+
+
+def unusual_five():
+   return (int(True << True << True ^ True))
+
+
+def unusual_five():
+   return ord("f") % ord("a")
+
+
+print(unusual_five(), 5)
