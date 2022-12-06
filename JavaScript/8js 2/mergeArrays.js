b94f139fbb9875d32ab10a7226fa31344040c3b5
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-12-06 11:33:55 +0700

    182.141.181

---
+++ JavaScript/8js 2/mergeArrays.js
@@ -0,0 +1,31 @@
+/*
+|--------------------------------------------------------------------------
+| Merge two sorted arrays into one
+|--------------------------------------------------------------------------
+|
+*/
+
+function mergeArrays(arr1, arr2) {
+   return [...new Set(arr1.concat(arr2).sort((a, b) => a - b))];
+}
+
+
+function mergeArrays(arr1, arr2) {
+   return arr1
+      .filter((item) => !arr2.includes(item))
+      .concat(arr2)
+      .sort((a, b) => a - b);
+}
+
+
+function mergeArrays(arr1, arr2) {
+   return Array.from(new Set([...arr1, ...arr2])).sort((a, b) => a - b);
+}
+
+
+console.log(mergeArrays([1, 2, 3, 4], [5, 6, 7, 8]), [1, 2, 3, 4, 5, 6, 7, 8]);
+console.log(mergeArrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(
+   mergeArrays([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12]),
+   [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+);
