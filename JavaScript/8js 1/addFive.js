077e2e91d4e1781f8bdc290caf9985db0ad7fb1c
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-12-08 10:45:07 +0700

    184.141.183

---
+++ JavaScript/8js 1/addFive.js
@@ -0,0 +1,23 @@
+/*
+|--------------------------------------------------------------------------
+| Grasshopper - Basic Function Fixer
+|--------------------------------------------------------------------------
+|
+*/
+
+function addFive(num) {
+   return num + 5;
+}
+
+
+addFive = (ॱ) => 5 + ॱ;
+
+
+function addFive(_) {
+   return -~-~-~-~-~_;
+}
+
+
+console.log(addFive(5), 10);
+console.log(addFive(0), 5);
+console.log(addFive(-5), 0);
