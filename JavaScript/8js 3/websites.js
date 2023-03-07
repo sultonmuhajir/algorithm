27bca795954057ae408f8d412784afb28757f6ad
Sulton Muhajir <sulton.muhajir28@gmail.com>
2023-03-07 19:18:14 +0700

    239.149.238

---
+++ JavaScript/8js 3/websites.js
@@ -0,0 +1,19 @@
+/*
+|--------------------------------------------------------------------------
+| Kata Example Twist
+|--------------------------------------------------------------------------
+|
+*/
+
+let websites = new Array(1000).fill("codewars");
+
+
+let websites = [];
+while (websites.length < 1000) websites.push("codewars");
+
+
+let websites = [];
+for (let i = 0; i < 1000; i++) websites.push("codewars");
+
+
+console.log(websites);
