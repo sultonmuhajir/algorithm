68b50ba700c83fff78ee403a8320b4cbf23f2323
Sulton Muhajir <sulton.muhajir28@gmail.com>
2023-03-08 08:31:26 +0700

    239.149.239

---
+++ Python/8py 3/websites.py
@@ -0,0 +1,22 @@
+"""
+|--------------------------------------------------------------------------
+| Kata Example Twist
+|--------------------------------------------------------------------------
+|
+"""
+
+websites = ['codewars' for i in range(1000)]
+
+
+websites = []
+while len(websites) < 1000: websites += ['codewars']
+
+
+websites = ["codewars"] * 1000
+
+
+websites = []
+for i in range(1000): websites.append("codewars")
+
+
+print(websites)
