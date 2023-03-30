9a91230efb83f5cb6e8de7c40065cfb28a01e478
Sulton Muhajir <sulton.muhajir28@gmail.com>
2023-03-30 13:52:10 +0700

    254.149.253

---
+++ JavaScript/8js 1/divide_numbers.js
@@ -0,0 +1,25 @@
+/*
+|--------------------------------------------------------------------------
+| Incorrect division method
+|--------------------------------------------------------------------------
+|
+*/
+
+const solve = (x, y) => x / y;
+
+
+function solve(x, y) {
+   return x / y;
+}
+
+
+const solve = require("ramda").divide;
+
+
+console.log(solve(2, 1), 2);
+console.log(solve(2, 4), 0.5);
+console.log(solve(42, 0), Infinity);
+console.log(solve(0, 8), 0);
+console.log(solve(7, 2), 3.5);
+console.log(solve(4.2, 1), 4.2);
+console.log(solve(9999, 11), 909);
