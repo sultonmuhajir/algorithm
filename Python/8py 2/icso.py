1657a269329dfb6b0e55954b13fe23a443b6409e
Sulton Muhajir <sulton.muhajir28@gmail.com>
2023-04-12 16:53:49 +0700

    262.149.262

---
+++ Python/8py 2/icso.py
@@ -0,0 +1,24 @@
+"""
+|--------------------------------------------------------------------------
+| Chuck Norris VII - True or False? (Beginner)
+|--------------------------------------------------------------------------
+|
+"""
+
+def if_chuck_says_so():
+   return not True
+
+
+def ifChuckSaysSo():
+   return 'a' == 'b'
+
+
+def ifChuckSaysSo():
+   return bool(0)
+
+
+def ifChuckSaysSo():
+   return 5 > 10
+
+
+print(if_chuck_says_so(), False)
