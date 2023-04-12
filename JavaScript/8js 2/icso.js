9c5a0903cb6852978bea62f405cb3ecdb61acfa9
Sulton Muhajir <sulton.muhajir28@gmail.com>
2023-04-12 16:53:01 +0700

    262.149.161

---
+++ JavaScript/8js 2/icso.js
@@ -0,0 +1,28 @@
+/*
+|--------------------------------------------------------------------------
+| Chuck Norris VII - True or False? (Beginner)
+|--------------------------------------------------------------------------
+|
+*/
+
+function ifChuckSaysSo() {
+   return !true;
+}
+
+
+function ifChuckSaysSo() {
+   return 5 > 10;
+}
+
+
+function ifChuckSaysSo() {
+   return Boolean(0);
+}
+
+
+function ifChuckSaysSo() {
+   return "a" == "b";
+}
+
+
+console.log(ifChuckSaysSo(), false);
