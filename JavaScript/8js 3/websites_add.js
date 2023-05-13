f437bed08d7f9e45acdcd327ecffce00de3d0b96
Sulton Muhajir <sulton.muhajir28@gmail.com>
2023-05-13 18:43:13 +0700

    267.149.266

---
+++ JavaScript/8js 3/websites_add.js
@@ -0,0 +1,15 @@
+/*
+|--------------------------------------------------------------------------
+| Basic Training: Add item to an Array
+|--------------------------------------------------------------------------
+|
+*/
+
+let websites = [];
+websites.push("codewars");
+
+
+websites[0] = "codewars";
+
+
+websites.unshift("codewars");
