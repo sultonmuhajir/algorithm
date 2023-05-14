9456a5a2d8f3247b1e1a612d2af3e0d961d35382
Sulton Muhajir <sulton.muhajir28@gmail.com>
2023-05-14 12:39:34 +0700

    267.149.267

---
+++ Python/8py 3/websites_add.py
@@ -0,0 +1,15 @@
+"""
+|--------------------------------------------------------------------------
+| Basic Training: Add item to an Array
+|--------------------------------------------------------------------------
+|
+"""
+
+websites = []
+websites.append('codewars')
+
+
+websites+=["codewars"]
+
+
+websites.insert(0, "codewars")
