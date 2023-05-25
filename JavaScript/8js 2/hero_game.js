2316bf1f84619d1234316e608447c645793826c9
Sulton Muhajir <sulton.muhajir28@gmail.com>
2023-05-25 10:31:09 +0700

    276.149.275

---
+++ JavaScript/8js 2/hero_game.js
@@ -0,0 +1,27 @@
+/*
+|--------------------------------------------------------------------------
+| Grasshopper - Terminal Game #1
+|--------------------------------------------------------------------------
+|
+*/
+
+function Hero(name) {
+   return {
+      name: name || "Hero",
+      position: "00",
+      health: 100,
+      damage: 5,
+      experience: 0,
+   };
+}
+
+
+class Hero {
+   constructor(name = `Hero`) {
+      this.name = name;
+      this.position = `00`;
+      this.health = 100;
+      this.damage = 5;
+      this.experience = 0;
+   }
+}
